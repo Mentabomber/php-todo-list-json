cbf446be17c233cc1a2e1274042505687c3312c4
Menta <simcictilen.gmail.com>
2023-06-22 16:04:52 +0200

    front end macro

--- front-end/my-vue-app/src/App.vue
+++ front-end/my-vue-app/src/App.vue
@@ -9,33 +9,46 @@ export default{
       newTask: "",
     };
   },
-}
-methods: {
-  onSubmit() {
+  methods: {
+    
+    onSubmit() {
 
-    const url = "http://localhost/php/tasks.php";
-    const data = this.newTask;
-    const headers = {
-      headers: {'Content-Type': 'multipart/form-data'}
-    };
-    axios.post(url, data, headers)
-      .then (response => {
+      const url = "http://localhost/php/tasks.php";
+      const data = this.newTask;
+      const headers = {
+        headers: {'Content-Type': 'multipart/form-data'}
+      };
+      axios.post(url, data, headers)
+        .then (response => {
+          this.tasks = response.data;
+          this.newTask = "";
+        })
+        .catch(error => console.error("error", error));
+    }
+
+  },
+  mounted() {
+    axios.get('http//localhost/php/index.php')
+      .then(response => {
         this.tasks = response.data;
-        this.newTask = "";
-      })
-      .catch(error => console.error("error", error));
+      });
   }
-
-},
-mounted() {};
-
+}
 </script>
 
 <template>
   <h1>To do List</h1>
   <div class="container">
-    <!-- <div></div> v-for -->
+    <div v-for="task in tasks">
+      <span>{{ task }}</span>
+    </div> 
   </div>
+  <form @submit.prevent="onSubmit">
+      <label for="task">task </label>
+      <input type="text" name="task" id="task" v-model="newTask">
+      <br />
+      <input type="submit" value="CREATE NEW Task">
+    </form>
 </template>
 
 <style scoped>
