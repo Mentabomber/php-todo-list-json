260410d4d79025711ebe34710ef0eaf062b42de9
Menta <simcictilen.gmail.com>
2023-06-22 15:56:53 +0200

    first commit

---
+++ front-end/my-vue-app/src/App.vue
@@ -0,0 +1,43 @@
+<script >
+import AppMain from './components/AppMain.vue';
+import axios from 'axios';
+
+export default{
+  data(){
+    return{
+      tasks: [],
+      newTask: "",
+    };
+  },
+}
+methods: {
+  onSubmit() {
+
+    const url = "http://localhost/php/tasks.php";
+    const data = this.newTask;
+    const headers = {
+      headers: {'Content-Type': 'multipart/form-data'}
+    };
+    axios.post(url, data, headers)
+      .then (response => {
+        this.tasks = response.data;
+        this.newTask = "";
+      })
+      .catch(error => console.error("error", error));
+  }
+
+},
+mounted() {};
+
+</script>
+
+<template>
+  <h1>To do List</h1>
+  <div class="container">
+    <!-- <div></div> v-for -->
+  </div>
+</template>
+
+<style scoped>
+
+</style>
